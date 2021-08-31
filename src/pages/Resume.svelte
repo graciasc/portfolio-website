42863fc8d0f9cada35a1c2a1ea6dfa068b1df451
Gracias <gracias.claude@outlook.com>
2021-08-31 00:07:10 -0600

    add image for resume page

--- src/pages/Resume.svelte
+++ src/pages/Resume.svelte
@@ -1,3 +1,9 @@
+<script>
+
+ let resumeImg = "image/resume.png";
+  </script>
 <main>
   <h1>Resume page</h1>
+  <!-- Image -->
+  <img src='https://personal-foto.s3.us-west-1.amazonaws.com/resume.png' alt="resume"/>  
 </main>
